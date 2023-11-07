e747325f8d6b5162618bb13341d1bd5cd28493d0
Wonseok Shin <gratus907@snu.ac.kr>
2023-11-07 17:05:53 +0900

    clean repository

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ -pthread -w -std=c++11 -O3 -DNDEBUG -DDEGORDER -DFIRSTASC -DSECONDASC  -I /extdata3/parklab/dna/include -L /extdata3/parklab/dna/lib DAF_div.cpp -o daf_10sec -lsdsl -ldivsufsort -ldivsufsort64
+g++ -pthread -w -std=c++11 -O3 -DNDEBUG -DDEGORDER -DFIRSTASC -DSECONDASC DAF_div.cpp -o daf_10sec -lsdsl -ldivsufsort -ldivsufsort64
